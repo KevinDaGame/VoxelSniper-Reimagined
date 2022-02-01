b83123d863bffda1a531913038af4637e9cbe5e2
KevinDaGame <kevindolfing2@gmail.com>
2022-02-02 00:05:21 +0100

    fix

--- src/main/java/com/thevoxelbox/voxelsniper/util/BlockHelper.java
+++ src/main/java/com/thevoxelbox/voxelsniper/util/BlockHelper.java
@@ -19,7 +19,7 @@ import org.bukkit.entity.Player;
 public class BlockHelper {
 
     private static final int MAXIMUM_WORLD_HEIGHT = 255;
-    private static final int MINIMUM_WORLD_HEIGHT = ;
+    private static final int MINIMUM_WORLD_HEIGHT = -64;
     private static final double DEFAULT_PLAYER_VIEW_HEIGHT = 1.65;
     private static final double DEFAULT_LOCATION_VIEW_HEIGHT = 0;
     private static final double DEFAULT_STEP = 0.2;
