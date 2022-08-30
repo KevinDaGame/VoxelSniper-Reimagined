128cbe350b0c34fb95be1322482cc2a6763dea45
KevinDaGame <kevindolfing2@gmail.com>
2022-08-30 20:49:28 +0200

    Fixed double block removal on rightclick on block

--- src/main/java/com/thevoxelbox/voxelsniper/bukkit/VoxelSniperListener.java
+++ src/main/java/com/thevoxelbox/voxelsniper/bukkit/VoxelSniperListener.java
@@ -7,11 +7,19 @@ import com.thevoxelbox.voxelsniper.voxelsniper.material.VoxelMaterial;
 import com.thevoxelbox.voxelsniper.voxelsniper.player.AbstractPlayer;
 import com.thevoxelbox.voxelsniper.voxelsniper.player.BukkitPlayer;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EquipmentSlot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 /**
@@ -21,6 +29,7 @@ public class VoxelSniperListener implements Listener {
 
     private static final String SNIPER_PERMISSION = "voxelsniper.sniper";
     private final BukkitVoxelSniper plugin;
+    private List<UUID> cooldown = new ArrayList<>();
 
     /**
      * @param plugin
@@ -37,21 +46,21 @@ public class VoxelSniperListener implements Listener {
         if(event.getHand() != EquipmentSlot.HAND) return;
         AbstractPlayer player = new BukkitPlayer(event.getPlayer());
 
-        if (!player.hasPermission(SNIPER_PERMISSION)) {
-            return;
-        }
-
+        if (!player.hasPermission(SNIPER_PERMISSION)) return;
+        if(cooldown.contains(player.getUniqueId())) return;
         try {
             Sniper sniper = VoxelProfileManager.getInstance().getSniperForPlayer(player);
             if (sniper.isEnabled() && sniper.snipe(
                     event.getAction(),
                     VoxelMaterial.getMaterial(event.getMaterial().getKey().getKey()),
-                    event.getClickedBlock() == null ? null : new BukkitBlock(event.getClickedBlock()),
+                    event.getClickedBlock() != null ? new BukkitBlock(event.getClickedBlock()) : null,
                     BlockFace.valueOf(event.getBlockFace().name()))) {
                 event.setCancelled(true);
+                cooldown.add(player.getUniqueId());
+                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> cooldown.remove(player.getUniqueId()),1);
             }
         } catch (final Exception temp) {
-temp.printStackTrace();
+            temp.printStackTrace();
         }
     }
 
@@ -66,5 +75,9 @@ temp.printStackTrace();
         if (player.hasPermission(SNIPER_PERMISSION) && plugin.getVoxelSniperConfiguration().isMessageOnLoginEnabled()) {
             sniper.displayInfo();
         }
+    }
+    @EventHandler
+    public final void onPlayerLeave(final PlayerQuitEvent event) {
+        cooldown.remove(event.getPlayer().getUniqueId());
     }
 }
