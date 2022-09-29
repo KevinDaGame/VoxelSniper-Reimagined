36353903d4a6e4d7dbca6727b1d76ac161be58ab
Lennart99 <le.le@live.nl>
2022-09-29 19:54:30 +0200

    Move VoxelSniperCore dependency to "voxel-implementation" plugin

--- VoxelSniperSpigot/build.gradle.kts
+++ VoxelSniperSpigot/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
 
     implementation("net.kyori:adventure-platform-bukkit:4.1.1")
-    implementation(project(":VoxelSniperCore"))
 
     testImplementation("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
 }
