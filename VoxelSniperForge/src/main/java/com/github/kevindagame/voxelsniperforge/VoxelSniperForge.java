828881ab9360c4ce0fda23f48191fce28ff7ab08
KevDaDev <kevindolfing2@gmail.com>
2022-09-29 15:29:42 +0200

    fix ModId

--- VoxelSniperForge/src/main/java/com/github/kevindagame/voxelsniperforge/VoxelSniperForge.java
+++ VoxelSniperForge/src/main/java/com/github/kevindagame/voxelsniperforge/VoxelSniperForge.java
@@ -30,7 +30,7 @@ import org.slf4j.Logger;
 public class VoxelSniperForge {
 
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "VoxelSniperForge";
+    public static final String MODID = "voxelsniperforge";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "VoxelSniperForge" namespace
