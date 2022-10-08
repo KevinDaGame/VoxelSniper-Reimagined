37b5772c0c49378b90bcc324fa220c35208a35c3
Lennart99 <le.le@live.nl>
2022-10-08 12:51:04 +0200

    made it possible to get a StringList from yaml and give a default String when using YamlConfiguration::getString

--- VoxelSniperCore/src/test/java/com/github/kevindagame/voxelsniper/YamlTest.java
+++ VoxelSniperCore/src/test/java/com/github/kevindagame/voxelsniper/YamlTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class YamlTest {
     private final File resourcesDirectory = new File("src/test/resources");
@@ -15,6 +16,7 @@ public class YamlTest {
         YamlConfiguration config = new YamlConfiguration(f);
 
         Assert.assertNull(config.getString("nonexistent"));
+        Assert.assertEquals("default3763654", config.getString("nonexistent.test", "default3763654"));
         Assert.assertSame(42, config.getInt("nonexistent", 42));
         Assert.assertTrue(config.getBoolean("nonexistent", true));
         Assert.assertFalse(config.getBoolean("nonexistent", false));
@@ -23,5 +25,12 @@ public class YamlTest {
         Assert.assertSame(15, config.getInt("test.testInt", 0));
         Assert.assertFalse(config.getBoolean("test.bool.testFalse", true));
         Assert.assertTrue(config.getBoolean("test.bool.testTrue", false));
+
+        ArrayList<String> lst = new ArrayList<>();
+        lst.add("test");
+        lst.add("test5");
+        lst.add("test42");
+        lst.add("5");
+        Assert.assertEquals(config.getStringList("test.testList"), lst);
     }
 }
