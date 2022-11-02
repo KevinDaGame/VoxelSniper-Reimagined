3a82e6185663c007b3deb18eeaf95017a153eee6
Lennart99 <le.le@live.nl>
2022-11-02 19:15:55 +0100

    Implemented EventPriority

--- VoxelSniperCore/src/main/java/com/github/kevindagame/voxelsniper/events/HandlerList.java
+++ VoxelSniperCore/src/main/java/com/github/kevindagame/voxelsniper/events/HandlerList.java
@@ -1,16 +1,18 @@
 package com.github.kevindagame.voxelsniper.events;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Consumer;
 
 public class HandlerList<T extends Event> {
-	private final List<Consumer<T>> handlers = new ArrayList<>();
-	
-	public void registerListener(Consumer<T> handler) {
-		handlers.add(handler);
+	// Using a TreeSet to force the handlers to be sorted
+	private final Set<EventListener<T>> handlers = new TreeSet<>();
+	public void registerListener(EventPriority priority, Consumer<T> handler) {
+		this.handlers.add(new EventListener<>(priority, handler));
 	}
-	
+	public void registerListener(Consumer<T> handler) {
+		this.registerListener(EventPriority.NORMAL, handler);
+	}
 	public void callEvent(T ev) {
 		for(Consumer<T> handler : handlers) {
 			handler.accept(ev);
