82fca79c28f7e86cd1a08af4fd5801431d8f33eb
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2022-12-25 11:43:51 +0100

    Correct kotlin jvm targets

--- VoxelSniperCore/build.gradle.kts
+++ VoxelSniperCore/build.gradle.kts
@@ -13,9 +13,9 @@ repositories {
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "16"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "16"
 }
--- VoxelSniperSpigot/build.gradle.kts
+++ VoxelSniperSpigot/build.gradle.kts
@@ -60,9 +60,9 @@ repositories {
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "16"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "16"
 }
