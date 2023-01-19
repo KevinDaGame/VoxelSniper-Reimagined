da8b7ea5b69a86f35b83f0b61f7b13362944b4e5
Lennart99 <le.le@live.nl>
2023-01-19 09:19:31 +0100

    #112: Fixed NPE when joining server without voxelsniper permissions (still needs to be validated) (#113)

--- VoxelSniperCore/src/main/java/com/github/kevindagame/command/VoxelBrushCommand.java
+++ VoxelSniperCore/src/main/java/com/github/kevindagame/command/VoxelBrushCommand.java
@@ -58,7 +58,10 @@ public class VoxelBrushCommand extends VoxelCommand {
         try {
             int originalSize = snipeData.getBrushSize();
 
-            if (!new PlayerBrushSizeChangedEvent(player, currentToolId, sniper.getBrush(currentToolId), originalSize, snipeData.getBrushSize()).callEvent().isCancelled()) {
+            var brush = sniper.getBrush(currentToolId);
+            if (brush == null) return false;
+
+            if (!new PlayerBrushSizeChangedEvent(player, currentToolId, brush, originalSize, snipeData.getBrushSize()).callEvent().isCancelled()) {
                 snipeData.setBrushSize(Integer.parseInt(args[0]));
                 snipeData.getVoxelMessage().size();
                 return true;
