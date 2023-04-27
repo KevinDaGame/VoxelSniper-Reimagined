95104a88257841d9bd803fa7d08c963e1fc67219
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2023-04-27 16:50:19 +0200

    Second Prototype for snow brush

--- VoxelSniperCore/src/main/java/com/github/kevindagame/brush/SnowBrush.kt
+++ VoxelSniperCore/src/main/java/com/github/kevindagame/brush/SnowBrush.kt
@@ -12,9 +12,9 @@ import com.github.kevindagame.voxelsniper.material.VoxelMaterial
  * @author KevDaDev
  */
 class SnowBrush : AbstractBrush() {
-    private var checkRange = 5
+    private var checkRange = 7
     override fun arrow(v: SnipeData) {
-        stackSnow(v)
+        stackSnowTest(v)
     }
 
     override fun powder(v: SnipeData) {
@@ -22,6 +22,26 @@ class SnowBrush : AbstractBrush() {
     }
 
     override fun info(vm: VoxelMessage) {}
+    private fun stackSnowTest(v: SnipeData) {
+        val block = lastBlock
+        if (!(block.material.isAir || block.material.equals(VoxelMaterial.SNOW)) || !block.getRelative(
+                0,
+                -1,
+                0
+            ).material.isSolid
+        ) return
+        val averageHeight = getAverageHeight(block)
+        var layers = (averageHeight * 8).toInt()
+        val blockData = VoxelMaterial.SNOW.createBlockData() as ISnow
+        blockData.setLayers(
+            layers.coerceIn(
+                blockData.getMinimumLayers(),
+                blockData.getMaximumLayers()
+            )
+        )
+        addOperation(BlockOperation(block.location, block.blockData, blockData))
+    }
+
     private fun stackSnow(v: SnipeData) {
         val positions = Shapes.ball(
             targetBlock.location,
@@ -37,17 +57,6 @@ class SnowBrush : AbstractBrush() {
             ) continue
             val averageHeight = getAverageHeight(block)
             var layers = (averageHeight * 8).toInt()
-            if (block.material.equals(VoxelMaterial.SNOW)) layers++
-            if (layers >= 8) {
-                addOperation(
-                    BlockOperation(
-                        block.location,
-                        block.blockData,
-                        VoxelMaterial.SNOW_BLOCK.createBlockData()
-                    )
-                )
-                continue
-            }
             val blockData = VoxelMaterial.SNOW.createBlockData() as ISnow
             blockData.setLayers(
                 layers.coerceIn(
@@ -60,24 +69,52 @@ class SnowBrush : AbstractBrush() {
     }
 
     private fun getAverageHeight(block: IBlock): Double {
-        var heightSum = 0.0
-        var count = 0
+        //find the nearest block that slopes up (is 1 block higher than block)
+        // look along the x and z axis for the nearest block that slopes up
+        // If found, you can stop
+        // If not found, expand the search radius by 1 block, until MAX_CHECK_RANGE is reached
+        var closestHigherBlock: IBlock? = null
+        var closestHigherDistance = checkRange.toDouble()
+        var closestLowerBlock: IBlock? = null
+        var closestLowerDistance = checkRange.toDouble()
+
         for (x in -checkRange..checkRange) {
             for (z in -checkRange..checkRange) {
-                if (x == 0 && z == 0) continue
-                val rel = block.getRelative(x, 0, z)
-                if (rel.blockData is ISnow) {
-                    val snow = rel.blockData as ISnow
-                    heightSum += snow.getLayers() / 8.0
-                    count++
-                    continue
+                val relativeBlock = block.getRelative(x, 0, z)
+                val dist = block.location.distance(relativeBlock.location)
+                if (relativeBlock.material.isSolid && !relativeBlock.material.equals(VoxelMaterial.SNOW)) {
+                    if (dist < closestHigherDistance) {
+                        closestHigherBlock = relativeBlock
+                        closestHigherDistance = dist
+                    }
+                } else if ((relativeBlock.material.isAir) && (relativeBlock.getRelative(
+                        0,
+                        -1,
+                        0
+                    ).material.isAir || relativeBlock.getRelative(0, -1, 0).material.equals(VoxelMaterial.SNOW))
+                ) {
+                    if (dist < closestLowerDistance) {
+                        closestLowerBlock = relativeBlock
+                        closestLowerDistance = dist
+                    }
                 }
-                if (rel.material.isSolid) {
-                    heightSum += 1
-                }
-                count++
             }
         }
-        return heightSum / count
+
+        if (closestHigherBlock == null && closestLowerBlock == null) return 0.125
+        val totalDistance = closestHigherDistance + closestLowerDistance
+        //if there is a lower block right next to it. return .125
+        //if there is no higher block, return 0.125
+        return if (totalDistance == 0.0) {
+            // avoid division by zero
+            return 0.0
+        }else if (closestHigherBlock == null) {
+            0.125
+        } else if (closestLowerDistance == 1.0){
+            0.125
+        } else {
+            closestLowerDistance / totalDistance
+        }
+
     }
 }
